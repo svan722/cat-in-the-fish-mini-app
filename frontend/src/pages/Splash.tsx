7d11bc2381b4a21bf3b77a9afbf6035508da9bbe
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-10-09 09:12:34 -0700

    remove background on splash

--- frontend/src/pages/Splash.tsx
+++ frontend/src/pages/Splash.tsx
@@ -1,3 +1,5 @@
+import "./splash.css";
+
 const Splash = ({ onClick }: { onClick: () => void }) => {
     return <div onClick={onClick} className="absolute cursor-pointer w-screen h-screen bg-[url('/imgs/splash.png')] bg-cover bg-center">
         <h1 className="text-center text-[#F1F3B1] font-extrabold text-[25px] mt-[60vh]">Please tap to continue</h1>
